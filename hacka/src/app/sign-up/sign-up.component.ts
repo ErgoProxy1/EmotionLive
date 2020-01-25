8de3d5e6ad12a5064424993be966d4a8d19ca157
Jaafar <jaafar.kaoussarani@polymtl.ca>
2020-01-25 16:49:59 -0500

    JK: Sign up functional

--- hacka/src/app/sign-up/sign-up.component.ts
+++ hacka/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm: FormGroup;
+
+  successMessage: String;
+  errorMessage: String;
+
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) { }
 
   ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      username: new FormControl('', [Validators.required, Validators.maxLength(30), Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(75), Validators.minLength(5)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(32)])
+    })
+  }
+
+  tryRegister(form: FormGroup) {
+    this.auth.doRegister(form.value.username, form.value.email, form.value.password)
+      .then(res => {
+        console.log(res);
+        console.log(this.auth.username)
+        this.successMessage = "Your account has been created";
+      }, err => {
+        console.log(err);
+        this.errorMessage = err.message;
+      })
   }
 
 }
--- hacka/src/services/auth.service.ts
+++ hacka/src/services/auth.service.ts
@@ -7,15 +7,42 @@ import * as firebase from 'firebase/app';
 })
 export class AuthService {
 
+  username: string;
+  errorMessage: string;
+
   constructor(public fbAuth: AngularFireAuth) {
 
   }
 
-  doRegister(email: string, password: string) {
+  doRegister(username: string, email: string, password: string) {
+    this.username = username;
     return new Promise<firebase.auth.UserCredential>((resolve, reject) => {
-      this.fbAuth.auth.createUserWithEmailAndPassword(email, password).then(res => {
-        resolve(res);
-      }, err => reject(err))
+      this.fbAuth.auth.createUserWithEmailAndPassword(email, password).then((signin) => {
+        this.updateUsername().then(()=>{
+          resolve(signin);
+        }).catch((username_error)=>{
+          reject(username_error)
+        })
+      }).catch((signin_error) => {
+        reject(signin_error)
+      });
+    })
+  }
+
+  updateUsername() {
+    return new Promise<String>((resolve, reject) => {
+      this.fbAuth.auth.onAuthStateChanged((user) => {
+        if (user) {
+          user.updateProfile({
+            displayName: this.username
+          }).then(() => {
+            this.username = user.displayName;
+            resolve("")
+          }).catch((err) => {
+            reject(err)
+          })
+        }
+      })
     })
   }
 
