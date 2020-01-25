5da631ec957e7b783d6a32ee0f2fa91edae52aaa
Weiliang-Xie <weiliangxie@hotmail.com>
2020-01-25 11:37:01 -0500

    Added FAQ and changed app/home

---
+++ hacka/src/app/about-faq/about-faq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-faq',
+  templateUrl: './about-faq.component.html',
+  styleUrls: ['./about-faq.component.scss']
+})
+export class AboutFaqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
