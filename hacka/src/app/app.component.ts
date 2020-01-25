a9a996f0c807b97c14a30d7be215d07aa1132cb2
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2020-01-25 06:53:08 -0500

    Replace the project's use of .css with .scss

--- hacka/src/app/app.component.ts
+++ hacka/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Right Now I Am';
