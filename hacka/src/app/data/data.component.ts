9dfc8a68158375cf55c6bb3816da0fc6ede29516
root <root@localhost.localdomain>
2020-01-25 15:15:29 -0500

    add data chart

--- hacka/src/app/data/data.component.ts
+++ hacka/src/app/data/data.component.ts
@@ -9,7 +9,34 @@ export class DataComponent implements OnInit {
 
   constructor() { }
 
+	title;
+	type;
+    data;
+    columnNames;
+    options;
+    width;
+	height;
+	
   ngOnInit() {
+    this.title = 'Emotions throughout the Day';
+    this.type = 'PieChart';
+    this.data = [
+      ['Firefox', 45.0],
+      ['IE', 26.8],
+      ['Chrome', 12.8],
+      ['Safari', 8.5],
+      ['Opera', 6.2],
+      ['Others', 0.7] 
+    ];
+    this.columnNames = ['Browser', 'Percentage'];
+    this.options = {   
+		pieHole: 0.4, 
+		chartArea: {width:'75%', height:'75%'},
+		top: 100,
+    };
+    this.width = 500;
+    this.height = 500;
   }
+  	
 
 }
