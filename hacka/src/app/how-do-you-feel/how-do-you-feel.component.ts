58dfe37730fc5df1efb50188ea1de716bbd70a96
Weiliang-Xie <weiliangxie@hotmail.com>
2020-01-25 16:20:06 -0500

    button is disabled when no emotion is chosen

--- hacka/src/app/how-do-you-feel/how-do-you-feel.component.ts
+++ hacka/src/app/how-do-you-feel/how-do-you-feel.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { EmotionChoice } from '../classes/emotionChoice';
 import { EmotionChoices } from '../consts/emotionChoices';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-how-do-you-feel',
-  templateUrl: './how-do-you-feel.component.html',
-  styleUrls: ['./how-do-you-feel.component.scss']
+    selector: 'app-how-do-you-feel',
+    templateUrl: './how-do-you-feel.component.html',
+    styleUrls: ['./how-do-you-feel.component.scss']
 })
+
 export class HowDoYouFeelComponent implements OnInit {
-  emotionChoices: EmotionChoice[] = EmotionChoices;
-  selectedEmotion: EmotionChoice;
+    emotionChoices: EmotionChoice[] = EmotionChoices;
+    selectedEmotion: EmotionChoice;
 
-  constructor() { }
+    constructor(private router : Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  selectEmotion(emotion: EmotionChoice): void {
-    this.selectedEmotion == emotion ? this.selectedEmotion = null : this.selectedEmotion = emotion;
-  }
+    selectEmotion(emotion: EmotionChoice): void {
+        this.selectedEmotion == emotion ? this.selectedEmotion = null : this.selectedEmotion = emotion;
+        console.log(this.selectedEmotion);
+    }
+
+    clickNext() : void {
+        this.router.navigateByUrl(`why-do-you-feel/${this.selectedEmotion.name}`)
+    }
 }
