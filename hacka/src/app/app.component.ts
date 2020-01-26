dd35cfbad0fabce103844c7d5906dbd8040e2037
Jaafar <jaafar.kaoussarani@polymtl.ca>
2020-01-26 07:12:41 -0500

    JK: Made redirect to home on refresh

--- hacka/src/app/app.component.ts
+++ hacka/src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from 'src/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Right Now I Am';
 
-  constructor(private db: AngularFirestore, private auth: AuthService) {
+  constructor(private db: AngularFirestore, private auth: AuthService, private router: Router) {
     
+  }
+
+  ngOnInit(){
+    this.router.navigate([''])
   }
 
 }
